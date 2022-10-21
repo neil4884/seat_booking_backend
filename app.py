88912e0e72bbb15f3a4f6f00c214866f3ff69192
vivatsathornthitasirivit <neil4884@gmail.com>
2022-10-21 21:23:50 +0700

    Added REST API

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from unit import User, Seat
 import json
 
 app = Flask(__name__)
@@ -9,12 +10,31 @@ def home():  # put application's code here
     return 'Hello World!'
 
 
-@app.route('/test')
+@app.route('/test', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def test_page():
-    q = request.args.get('q')
-    print(q)
-    return {'message': "Hello!"}, 201
+    """
+    This is an example of REST API usage, with addition of linkage to our database and linkage classes.
+
+    POST: Backend gets Data from Frontend
+    GET: Frontend requests and gets Data from Backend
+    PUT:
+    DELETE:
+    :return:
+    """
+    if request.method == 'POST':
+        body = request.data
+        data = json.loads(body, strict=False) if body else dict()
+        print(data)
+        return data, 201
+
+    elif request.method == 'GET':
+        q = request.args.get('q')
+        print(q)
+        return {'message': 'Hello!', 'loop_back': q}, 200
+
+    elif request.method == 'PUT':
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- unit.py
+++ unit.py
@@ -21,20 +21,36 @@ class User:
     def friends(self, value):
         self.__friends = value
 
+    @property
+    def status(self):
+        return self.__status
+
+    @status.setter
+    def status(self, value):
+        self.__status = value
+
+    @property
+    def seat_id(self):
+        return self.__seat_id
+
+    @seat_id.setter
+    def seat_id(self, value):
+        self.__seat_id = value
+
 
 class Seat:
     def __init__(self):
-        self.__id = ''
+        self.__seat_id = ''
         self.__status = 0
         self.__caption = ''
 
     @property
-    def id(self):
-        return self.__id
+    def seat_id(self):
+        return self.__seat_id
 
-    @id.setter
-    def id(self, value):
-        self.__id = value
+    @seat_id.setter
+    def seat_id(self, value):
+        self.__seat_id = value
         return
 
     @property
