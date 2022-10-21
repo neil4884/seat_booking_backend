8172a12ac31908c45d098ee3fee9c4deb1184026
vivatsathornthitasirivit <neil4884@gmail.com>
2022-10-21 13:13:39 +0700

    Added unit

---
+++ unit.py
@@ -0,0 +1,64 @@
+class User:
+    def __init__(self):
+        self.__id = ''
+        self.__friends = []
+        self.__status = 0
+        self.__seat_id = ''
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def friends(self):
+        return self.__friends
+
+    @friends.setter
+    def friends(self, value):
+        self.__friends = value
+
+
+class Seat:
+    def __init__(self):
+        self.__id = ''
+        self.__status = 0
+        self.__caption = ''
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+        return
+
+    @property
+    def status(self):
+        return self.__status
+
+    @status.setter
+    def status(self, value):
+        self.__status = value
+        return
+
+    @property
+    def caption(self):
+        return self.__caption
+
+    @caption.setter
+    def caption(self, value):
+        self.__caption = value
+
+
+if __name__ == '__main__':
+    seat = Seat()
+    user = User()
+    seat.id = 'xx'
+    print(seat.id)
+    user.friends.append('xxz')
+    print(user.friends)
--- user.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self):
-        self.__id = ''
-        self.__friends = []
-        self.__status = 0
-        self.__
