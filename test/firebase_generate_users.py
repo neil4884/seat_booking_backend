4edb7024345df67b68923fc4848bbca20da94ad7
vivatsathornthitasirivit <neil4884@gmail.com>
2022-10-26 15:59:51 +0700

    fb test

--- test/firebase_generate_users.py
+++ test/firebase_generate_users.py
@@ -8,25 +8,47 @@ from names import names
 
 random.seed(777778)
 
-cred = credentials.Certificate(os.path.join(os.path.dirname(__file__),
-                                            '../credentials/seat-booking-db-firebase-adminsdk-4e0g2-518adfa3cc.json'))
+cred_path = '../credentials/seat-booking-db-firebase-adminsdk-4e0g2-518adfa3cc.json'
+cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), cred_path))
 app_fb = fb.initialize_app(cred)
 db = firestore.client()
 
+list_seats = [
+    'F1-' + str(e) for e in ['A{}'.format(str(i).zfill(2)) for i in range(1, 9)] +
+    ['B{}'.format(str(i).zfill(2)) for i in range(1, 9)] +
+    ['C{}'.format(str(i).zfill(2)) for i in range(1, 65)]
+]
+
+list_users = [
+    '{}'.format(str(k).zfill(3)) for k in range(1, 101)
+]
+
 if __name__ == '__main__':
-    for k in range(1, 81):
-        sid = '643{}21'.format(str(k).zfill(5))
+    for user_id in list_users:
         name = random.choice(names) + ' ' + random.choice(names)
         u = {
-            'id': sid,
+            'id': user_id,
             'name': name,
-            'friends': ['643{}21'.format(str(i).zfill(5)) for i in random.choices(range(1, 81), k=8) if i != k],
+            'friends': [friend for friend in random.choices(list_users, k=4) if friend != user_id],
             'status': 0,
             'current_seat_id': '',
             'booked_time': datetime.datetime.now(),
-            'temp_time': datetime.datetime.now(),
-            'unique_id': hash(sid)
+            'temp_time': '',
+            'extend_time': '',
+            'caption': '',
+            'whatsup': ''
         }
-        user_doc_ref = db.collection('users').document(sid)
+        user_doc_ref = db.collection('users').document(user_id)
         user_doc_ref.set(u)
         print(u)
+
+    for seat_id in list_seats:
+        s = {
+            'id': seat_id,
+            'status': 0,
+            'seat_user': '',
+            'booked_time': datetime.datetime.now(),
+        }
+        user_doc_ref = db.collection('seat').document(seat_id)
+        user_doc_ref.set(s)
+        print(s)
--- test/firebase_test.py
+++ test/firebase_test.py
@@ -10,42 +10,30 @@ app_fb = fb.initialize_app(cred)
 db = firestore.client()
 
 if __name__ == '__main__':
-    for k in range(1, 1001):
-        id = '643{}21'.format(str(k).zfill(5))
+    for k in range(1, 101):
+        sid = '643{}21'.format(str(k).zfill(5))
         did = {
-            'id': id,
+            'id': sid,
             'friends': ['643{}21'.format(str(i).zfill(5)) for i in range(1, 11) if i != k],
             'status': 0,
             'current_seat_id': 'NO_SEAT',
             'check_in_time': datetime.datetime.now(),
-            'check_out_time': datetime.datetime.now()
+            'check_out_time': datetime.datetime.now(),
+            'caption': ''
         }
-        user_doc_ref = db.collection('users').document(id)
+        user_doc_ref = db.collection('users').document(sid)
         user_doc_ref.set(did)
-        print(id)
+        print(sid)
 
     for k in range(1, 51):
-        id = 'F01A{}'.format(str(k).zfill(3))
+        sid = 'F01A{}'.format(str(k).zfill(3))
         did = {
-            'id': id,
+            'id': sid,
             'status': 0,
-            'caption': '',
             'book_time': datetime.datetime.now(),
-            'seat_user': '643{}21'.format(str(400 - k).zfill(5))
+            'seat_user': ''
         }
-        seat_doc_ref = db.collection('seats').document(id)
+        seat_doc_ref = db.collection('seats').document(sid)
         seat_doc_ref.set(did)
-        print(id)
+        print(sid)
 
-    for k in range(51, 101):
-        id = 'F02A{}'.format(str(k).zfill(3))
-        did = {
-            'id': id,
-            'status': 0,
-            'caption': '',
-            'book_time': datetime.datetime.now(),
-            'seat_user': '643{}21'.format(str(800 - k).zfill(5))
-        }
-        seat_doc_ref = db.collection('seats').document(id)
-        seat_doc_ref.set(did)
-        print(id)
